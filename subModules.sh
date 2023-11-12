f3b1ce1f72b512076fdb9d7196c3ccb32d6b396a
Rohtash Lakra <rslakra@users.noreply.github.com>
2023-11-11 23:11:49 -0800

    updated subModules script to find folders and checkout master branch

--- subModules.sh
+++ subModules.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
 #Author:Rohtash Lakra
 clear
-# Step 1
+# Step 1 - initialize submodules recursively
 git submodule update --init --recursive
-# Step 2
+# Step 2 - - pull submodules recursively
 git pull --recurse-submodules
 echo
-# check-out master branch to avoid checkout of #head.
+# Step 3 - checkout master branch of submodules recursively (instead of #head branch)
 PROJECT_HOME=$PWD
 echo
 echo "Syncing [${PROJECT_HOME}] ..."
 echo
-SUB_MODULES=("Automobiles" "Business" "Commerce" "Commodity" "Education" "Healthcare" "Manufacturing" "Monitoring" "Multimedia" "Samples" "SpringSecurity" "Technology" "Transportation")
-echo "SubModules: ${SUB_MODULES}"
-# get length of an array
-totalSubModules=${#SUB_MODULES[@]}
-echo "totalSubModules: ${totalSubModules}"
 echo
 # use for loop to read all values and indexes
-for (( i=1; i<${totalSubModules}+1; i++ ));
+for dir in $(pwd)/*/;
 do
-  entry=${SUB_MODULES[$i-1]}
-  echo "Syncing [${i} / ${totalSubModules}] : [${entry}] ..."
-  cd ${entry}
+  dirName=$(basename "${dir}");
+  echo "Syncing [[${dirName}] ..."
+  cd ${dirName}
   git checkout master
   cd ..
   echo
