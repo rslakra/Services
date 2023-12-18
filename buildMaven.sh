1d24e10177da37f916633adf67f9d559c53441f0
Rohtash Lakra <rslakra@users.noreply.github.com>
2023-12-17 16:21:59 -0800

    code refactored and merged local changes

--- buildMaven.sh
+++ buildMaven.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
-#Author: Rohtash Lakra
+# Author: Rohtash Lakra
 clear
+VERSION="0.0"
+# Build Version Function
+function buildVersion() {
+  GIT_COMMIT_COUNT=$(git rev-list HEAD --count)
+  if [ $? -ne 0 ]; then
+    VERSION="${VERSION}.0"
+  else
+    VERSION="${VERSION}.${GIT_COMMIT_COUNT}"
+  fi
+  SNAPSHOT="${SNAPSHOT:-$!}"
+  if [[ ! -z ${SNAPSHOT} ]]; then
+      VERSION="${VERSION}-SNAPSHOT"
+  fi
+
+  echo "${VERSION}";
+}
+
 echo
-JAVA_VERSION=11
-export JAVA_HOME=$(/usr/libexec/java_home -v $JAVA_VERSION)
+#JAVA_VERSION=11
+#export JAVA_HOME=$(/usr/libexec/java_home -v $JAVA_VERSION)
 echo "${JAVA_HOME}"
 echo
-
-#mvn -X clean install
-mvn clean package
+#mvn clean install -DskipTests=true
+SNAPSHOT_VERSION=$(buildVersion SNAPSHOT)
+RELEASE_VERSION=$(buildVersion)
+mvn clean install -Drevision=$SNAPSHOT_VERSION -DskipTests=true
+mvn install -Drevision=$RELEASE_VERSION -DskipTests=true
+echo
--- runMaven.sh
+++ runMaven.sh
@@ -1,2 +1,11 @@
-./mvnw spring-boot:run
-
+#!/bin/bash
+#Author:Rohtash Lakra
+clear
+#echo
+#JAVA_VERSION=11
+#export JAVA_HOME=$(/usr/libexec/java_home -v $JAVA_VERSION)
+echo "${JAVA_HOME}"
+echo
+#mvn clean package -DskipTests
+mvn clean spring-boot:run
+#./mvnw spring-boot:run
